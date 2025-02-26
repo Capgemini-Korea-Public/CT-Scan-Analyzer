3c485294c6b26e1e75bb3c4cb2a8c4fc557d10ff
Ji Seung Do <86179438+seungdo1234@users.noreply.github.com>
2025-02-26 15:17:51 +0900

    Merge branch 'dev' into Feat/Command-System/SD

---
+++ Assets/Scripts/System/Command/CommandExample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+public class CommandExample : MonoBehaviour
+{
+    private CommandSystemManager commandManager;
+    private Transform targetObject;
+
+    void Start()
+    {
+        commandManager = GetComponent<CommandSystemManager>();
+        targetObject = transform;
+
+        // 커맨드 등록
+        commandManager.RegisterCommand("rotate", new RotateCommand(targetObject));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // 69도 회전
+            commandManager.ExecuteCommand("rotate", 69f);
+        }
+    }
+}
+
