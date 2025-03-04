02d7ab18faf2e43515642d16d9b614ce703be125
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-03-04 13:56:38 +0900

    [Fix] Image Reset Position Modified

--- Assets/Scripts/UI/InsertPhotoUI.cs
+++ Assets/Scripts/UI/InsertPhotoUI.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class InsertPhotoUI : MonoBehaviour
 {
     [SerializeField] private Button insertBtn;
     [SerializeField] private Image checkMark;
+    [SerializeField] private Image capturedImage;
 
     private void Start()
     {
@@ -21,5 +23,16 @@ public class InsertPhotoUI : MonoBehaviour
         bool b = checkMark.gameObject.activeInHierarchy;
         checkMark.gameObject.SetActive(!b);
         MainController.Instance.SetImageInserted(!b);
+        capturedImage.gameObject.SetActive(!b); 
+
+        //if (b)
+        //    ViewCapturedImage();
+    }
+
+    private void ViewCapturedImage()
+    {
+        Texture2D texture = MainController.Instance.CurCapturedImages[0];
+        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        capturedImage.sprite = sprite;
     }
 }
